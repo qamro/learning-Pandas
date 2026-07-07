1d15713721aeef22bc0777eddeeb5687c8fa8281
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-07 20:29:09 +0200

    series

--- series.py
+++ series.py
@@ -1,2 +1,6 @@
 import pandas as pd 
 
+# series are one-dimensional labeled arrays capable of holding any data type (integers, strings, floating point numbers, Python objects, etc.).
+# The axis labels are collectively referred to as the index.
+# A pandas Series can be created using the following constructor :
+# pandas.Series(data, index, dtype, name, copy)
