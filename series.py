7db9d554fcb876669f3538ae3897494d0ea457d8
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-07 20:55:36 +0200

    series

--- series.py
+++ series.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-# series are one-dimensional labeled arrays capable of holding any data type (integers, strings, floating point numbers, Python objects, etc.).
+# series are one-dimensional labeled arrays capable of holding any data type (integers, strings, floats, Python objects, etc.)
 # The axis labels are collectively referred to as the index.
 # A pandas Series can be created using the following constructor :
 # pandas.Series(data, index, dtype, name, copy)
@@ -17,5 +17,17 @@ print()
 # to access the elements of a Series
 # u can use the index labels 
 print(series[0])  # Accessing the first element
-# or you can use the index labels   
+print(series_with_index['a'])  # Accessing the element with index label 'a'
+# or you can use the index labels with loc method   
 print(series_with_index.loc['a'])  # Accessing the element with index label 'a'
+print()
+# to change a value in a Series, you can use the index label to assign a new value.
+series_with_index['a'] = 100
+series_with_index.loc['a'] = 100 # or you can use the loc method to assign a new value.
+print(series_with_index)
+print()
+# to change the index labels of a Series, you can use the index attribute.
+series_with_index.index = ['A', 'B', 'C', 'D', 'E']
+print(series_with_index)
+print()
+
