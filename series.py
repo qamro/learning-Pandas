8bb7425c51dfc892afd066636c81a2d3c14218f6
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-07 23:41:20 +0200

    series

--- series.py
+++ series.py
@@ -31,3 +31,24 @@ series_with_index.index = ['A', 'B', 'C', 'D', 'E']
 print(series_with_index)
 print()
 
+# FILTERING
+print(series_with_index[series_with_index > 100])  # Filtering elements greater than 100
+print(series_with_index[series_with_index > 200])  # Filtering elements greater than 200
+print(series_with_index[series_with_index < 200])  # Filtering elements less than 200
+print()
+
+
+
+# above we use a list as a data, now we will use a dictionary as a data to create a series.
+data_dict = {'a': 10, 'b': 234, 'c': 32, 'd': 443, 'e': 500}
+series_from_dict = pd.Series(data_dict)
+print(series_from_dict)
+# as u can see the index labels are taken from the keys of the dictionary by default.
+# and the data are taken from the values of the dictionary by default.
+# the accessing and filtering of the series created from a dictionary is the same as the series created from a list.
+print(series_from_dict['a'])  # Accessing the element with index label 'a'
+print(series_from_dict.loc['a'])  # Accessing the element with index label 'a'
+series_from_dict['a'] = 100
+series_from_dict.loc['a'] = 100 # or you can use the loc method to assign a new value.
+print(series_from_dict) 
+print(series_from_dict[series_from_dict > 100])  # Filtering elements greater than 100
