af3450261bbce8a0fdac01de20d00af464ac7add
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-08 00:11:37 +0200

    first commit

--- DataFrames.py
+++ DataFrames.py
@@ -1,1 +1,7 @@
 import pandas as pd
+
+# DataFrames are two-dimensional labeled data structures with columns of potentially different types.
+# You can think of them like a spreadsheet or SQL table, or a dict of Series objects.
+# It is generally the most commonly used pd object.
+# A pandas DataFrame can be created using the following constructor:
+# pandas.DataFrame(data, index, columns, dtype, copy)
