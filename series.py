5704e2523099729d490343e44c78247502b63158
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-07 20:46:02 +0200

    series

--- series.py
+++ series.py
@@ -7,7 +7,15 @@ import pandas as pd
 data = [10, 234, 32, 443, 500]
 series = pd.Series(data)
 print(series)
+print()
 # the index is by default an integer index starting from 0.
 # You can also specify your own index labels.
 series_with_index = pd.Series(data, index=['a', 'b', 'c', 'd', 'e'])
 print(series_with_index)
+print()
+# Note: that the index labels must be unique and of the same length as the data.
+# to access the elements of a Series
+# u can use the index labels 
+print(series[0])  # Accessing the first element
+# or you can use the index labels   
+print(series_with_index.loc['a'])  # Accessing the element with index label 'a'
