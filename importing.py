c2646191dad556b8bfe32aa7facf22b9afc4349a
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-10 16:30:20 +0200

    importing

--- importing.py
+++ importing.py
@@ -7,22 +7,25 @@ print(df)  # printing the DataFrame
 # if u have a very large data in the CSV file, it will show only the first 5 rows and the last 5 rows of the DataFrame.
 # if u want to print all the rows of the DataFrame, you can use the to_string() method.
 print(df.to_string()) # that's it 
+print()
 
 
 # JSON file
-df = pd.read_csv("students.csv")  #  reading a CSV file named "students.csv"
+df = pd.read_json("students.json")  #  reading a JSON file named "students.json"
 print(df)  # printing the DataFrame
 
-# if u have a very large data in the CSV file, it will show only the first 5 rows and the last 5 rows of the DataFrame.
+# if u have a very large data in the JSON file, it will show only the first 5 rows and the last 5 rows of the DataFrame.
 # if u want to print all the rows of the DataFrame, you can use the to_string() method.
 print(df.to_string()) # that's it 
+print()
 
 
 
-# CSV file
-df = pd.read_csv("students.csv")  #  reading a CSV file named "students.csv"
+# EXCEL file
+df = pd.read_excel("students.xlsx")  #  reading an EXCEL file named "students.xlsx"
 print(df)  # printing the DataFrame
 
-# if u have a very large data in the CSV file, it will show only the first 5 rows and the last 5 rows of the DataFrame.
+# if u have a very large data in the EXCEL file, it will show only the first 5 rows and the last 5 rows of the DataFrame.
 # if u want to print all the rows of the DataFrame, you can use the to_string() method.
 print(df.to_string()) # that's it 
+print()
