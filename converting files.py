ef3319362fd4ae0df1aec5355f01a9e79cb43733
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-10 20:27:58 +0200

    converting files using pandas

--- converting files.py
+++ converting files.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 # Create a simple DataFrame (rows have an index: 0, 1)
 df = pd.DataFrame({
-    "Name": ["Alice", "Bob"],
+    "Name": ["Qamro", "Scofield"],
     "Age": [20, 22],
-    "City": ["Paris", "London"]
+    "City": ["Sétif", "Fox River"]
 })
 
 
@@ -14,8 +14,8 @@ df = pd.DataFrame({
 
 # Converting the DataFrame to a CSV file
 
-df.to_csv("data.csv", index=False)      # Save to CSV (don't save the row index)
-df_csv = pd.read_csv("data.csv")        # Read CSV into a DataFrame
+df.to_csv("prison_break.csv", index=False)      # Save to CSV (don't save the row index)
+df_csv = pd.read_csv("prison_break.csv")        # Read CSV into a DataFrame
 
 
 
@@ -23,11 +23,13 @@ df_csv = pd.read_csv("data.csv")        # Read CSV into a DataFrame
 
 
 # Converting the DataFrame to a JSON file
+
 # when we convert a DataFrame to JSON, we can specify the orientation of the JSON data using the orient parameter.
 # NOTE: indent parameter is just for JSON files not the CSV files
 # and we add indent=4 to make the JSON file more readable.
 # because indent=4 adds 4 spaces of indentation to each level of the JSON data, making it easier to read and understand.
 # we can use indent=2 or indent=4 or any other number of spaces, depending on our preference.
+
 df.to_json("records.json", orient="records", indent=4)
 # records: list of dictionaries (most common in APIs, DATA exchanging...)
 # each dictionary represents one row, with column names as keys and row values as values.
@@ -36,7 +38,7 @@ df.to_json("index.json", orient="index", indent=4)
 # index: row index becomes the JSON key
 
 df.to_json("columns.json", orient="columns", indent=4)
-# columns: each column is a dictionary of index -> value (default)
+# columns: each column is a dictionary of index : value (default)
 
 df.to_json("values.json", orient="values", indent=4)
 # values: only the data, no column names
