8f2422847a694ac38e7ad348b5dfa8335804ec4b
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-10 16:19:22 +0200

    importing

--- importing.py
+++ importing.py
@@ -1,2 +1,9 @@
 import pandas as pd
 
+# CSV file
+df = pd.read_csv("students.csv")  #  reading a CSV file named "students.csv"
+print(df)  # printing the DataFrame
+
+# if u have a very large data in the CSV file, it will show only the first 5 rows and the last 5 rows of the DataFrame.
+# if u want to print all the rows of the DataFrame, you can use the to_string() method.
+print(df.to_string()) # that's it 
