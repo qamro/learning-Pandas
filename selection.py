8f7a2fd5d6f8a6cf6baada45fc0e99b7421ddec0
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-12 21:59:04 +0200

    selection

--- selection.py
+++ selection.py
@@ -3,8 +3,13 @@ import pandas as pd
 df = pd.read_csv("students.csv")
 print(df)
 print()
+
 # selection by column label
 print(df["Name"])  # Accessing the "Name" column
 print()
 print(df[["Name", "Age"]])  # Accessing multiple columns
+print()
+
+# selection by row index
+print(df.loc[0])  # Accessing the first row 
 print()
