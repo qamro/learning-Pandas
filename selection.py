3ac2e86e55fd852da8805028be833d6e4110675b
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-12 21:56:36 +0200

    selection

--- selection.py
+++ selection.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 df = pd.read_csv("students.csv")
 print(df)
+print()
+# selection by column label
+print(df["Name"])  # Accessing the "Name" column
+print()
+print(df[["Name", "Age"]])  # Accessing multiple columns
+print()
