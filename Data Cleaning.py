101971deac856ad6435f1feb6b1ead89b1f15f43
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-16 05:44:21 +0200

    data cleaning

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 # data cleaning is the process of fixing or removing incomplete, incorrect, or irrelevant data
 # NOTE: 75% of work done with pandas is data cleaning 
+
+df = pd.read_csv("students.csv")
+print(df)
+print()
