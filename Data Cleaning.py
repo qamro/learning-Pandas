df1b7cdcc077b7ac6983049f49d5acf9b5fc0528
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-17 23:17:51 +0200

    data cleaning

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -7,4 +7,25 @@ df = pd.read_csv("students.csv")
 print(df)
 print()
 
-# Drop irrelevant column
+
+
+
+# Drop irrelevant columns
+
+# Drop Country column
+df1 = df.drop(columns=["Country"])
+print(df1)
+print()
+# Drop Country and City columns
+df2 = df.drop(columns=["Country", "City"])
+print(df2)
+print()
+# Drop Country and City and Age columns
+df3 = df.drop(columns=["Country", "City", "Age"])
+print(df3)
+print()
+
+
+
+
+# 
