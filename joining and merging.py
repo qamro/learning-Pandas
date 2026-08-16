3e2811ef93af8325986f9b0d6b470733341fea36
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-08-16 20:56:08 +0200

    joining and merging

--- joining and merging.py
+++ joining and merging.py
@@ -1,16 +1,43 @@
 import pandas as pd 
 
-# Same key
-pd.merge(df1, df2, on="id")
+"""
+NOTE:
+merge()  → "Find matching keys between two DataFrames"
+join()   → "Combine mainly using indexes"
+concat() → "Put DataFrames together"
+"""
 
-# Different keys
-pd.merge(
-    df1,
-    df2,
-    left_on="student_id",
-    right_on="id"
-)
+"""
+NOTE: join() and merge() are almost the same the difference is:
+merge() → join using columns/keys and the key is generally on="id"
+join() → join using indexes
+"""
 
+# DataFrames
+df1 = pd.DataFrame({
+    "id": [1, 2, 3, 4],
+    "name": ["qamro", "raiden", "bassem", "raid"],
+    "city": ["Paris", "London", "Paris", "Madrid"]
+})
+
+df2 = pd.DataFrame({
+    "id": [3, 4, 5, 6],
+    "age": [21, 22, 20, 23]
+})
+
+print(df1)
+print(df2)
+
+
+
+
+
+# Merge
+"""
+NOTE: u should remove the id column from one the dataframes to make the pd.join() works 
+Because the pd.join() its the same as pd.merge() but pd.join() works on indexes 
+We shouldn't have any same column in the dataframes that we want to make a pd.join()
+"""
 # INNER
 pd.merge(df1, df2, on="id", how="inner")
 
@@ -23,22 +50,32 @@ pd.merge(df1, df2, on="id", how="right")
 # OUTER
 pd.merge(df1, df2, on="id", how="outer")
 
-# Multiple keys
-pd.merge(df1, df2, on=["id", "year"])
 
-# Index
-pd.merge(
-    df1,
-    df2,
-    left_index=True,
-    right_index=True
-)
+
+
+
 
 # Join
-df1.join(df2)
 
-# Stack rows
-pd.concat([df1, df2])
+# Left join
+df1.join(df2, how="left")
+
+# Right join
+df1.join(df2, how="right")
+
+# inner join
+df1.join(df2, how="inner")
+
+# outer join
+df1.join(df2, how="outer")
+
+
+
+
+# concatenate
+
+# Stack rows(concatenate rows)
+pd.concat([df1, df2]) # its axis=0 which means rows by default
 
 # Put columns side-by-side
-pd.concat([df1, df2], axis=1)
+pd.concat([df1, df2], axis=1) # axis=1 means columns
